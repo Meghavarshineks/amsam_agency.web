6a0582a228b7d4f65852bb5e1e6dc6864774ac7b
varshine <megha4401varshine@gmail.com>
2026-04-25 16:04:36 +0530

    Tile adhesive bug fixed

---
+++ final_cleanup.py
@@ -0,0 +1,13 @@
+﻿def fix(p, old_bytes, new_bytes):
+    import os
+    if not os.path.exists(p): return
+    with open(p, 'rb') as f: data = f.read()
+    if old_bytes in data:
+        data = data.replace(old_bytes, new_bytes)
+        with open(p, 'wb') as f: f.write(data)
+        print(f'Fixed {p}')
+    else: print(f'Already clean: {p}')
+
+fix(r'C:\Users\MH\OneDrive\Documents\VS Code\Amsam agency\cements.html', b'\xc3\xa2\xe2\x80\xa0\xe2\x80\x9c', '↓'.encode('utf-8'))
+fix(r'C:\Users\MH\OneDrive\Documents\VS Code\Amsam agency\tile-adhesive.html', b'\xc3\xa2\xe2\x80\xa0\xe2\x80\x9c', '↓'.encode('utf-8'))
+fix(r'C:\Users\MH\OneDrive\Documents\VS Code\Amsam agency\script.js', b'\xc3\xa2\xc5\xa1\xc2\xa0\xc3\xaf\xc2\xb8\xc2\x8f', '⚠️'.encode('utf-8'))
